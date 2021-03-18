015661bdd5c04343bd193702b5ae6c5bea086ab9
Ememme <magda.mieczynska@gmail.com>
2021-03-18 14:15:46 +0100

    Add code from lesson 3

---
+++ main.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from requests import api
+import time
+
+# WEBDRIVER:
+driver = webdriver.Chrome('./chromedriver')
+driver.get('https://fabrykatestow.pl')
+driver.close()
+
+
+# TERMINAL
+print('hello Magdalena')
+req = api.get('https://fabrykatestow.pl')
+time.sleep(5)
+print(req.status_code)
+time.sleep(5)
+print(req.headers['content-type'])
+time.sleep(5)
+print(req.encoding)
+time.sleep(2)
+print(req.json)
+
