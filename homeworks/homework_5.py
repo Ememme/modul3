95bb08772e5a3c845f8c05f9461504d1b34a09ab
Ememme <magda.mieczynska@gmail.com>
2021-04-30 14:13:23 +0200

    Refactor: variable names

--- homeworks/homework_5.py
+++ homeworks/homework_5.py
@@ -2,33 +2,46 @@ import requests
 import time
 import json
 import csv
-
+from datetime import datetime
 from params import api as API_KEY
 
 url = 'http://data.fixer.io/api/latest'
 
+
 def request_API_data(url):
     try:
         r = requests.get(url, params=API_KEY)
-        r_time = r.elapsed.total_seconds()
+        r_elapsed_time = r.elapsed.total_seconds()
+        r_hour = r.headers['date']
         response_data = json.loads(r.text)
-        return [response_data, r_time]
+        return [response_data, r_elapsed_time, r_hour]
     except requests.exceptions.Timeout:
-        print_connection_error_message()
+        print_timeout_error_message()
 
-def print_connection_error_message():
+
+def print_timeout_error_message():
     print('Brak połączenia z serwisem')
+
 
 def return_currency_data(currency):
     data = request_API_data(url)
     if data:
-        # print(data)
-        date = data[0]['date']
-        currency = data[0]['rates'][currency]
+        currency_date = data[0]['date']
+        currency_rate = data[0]['rates'][currency]
         request_time = data[1]
-        info = [currency, request_time, date]
-        # print(info)
-        return info
+        request_date = data[2]
+        formatted_date = format_time(request_date)
+        currency_info = [currency_date, currency_rate, request_time, formatted_date]
+        return currency_info
+    else:
+        print_timeout_error_message()
+
+
+def format_time(str):
+    request_time = str
+    time_format = "%a, %d %b %Y %H:%M:%S %Z"
+    formatted_time = datetime.strptime(request_time, time_format)
+    return formatted_time
 
 
 def write_csv():
@@ -36,6 +49,8 @@ def write_csv():
         writer = csv.writer(file)
         writer.writerows([info])
 
+
+# def get_PLN_rate():
 sample = 10
 
 while sample != 0:
@@ -43,15 +58,18 @@ while sample != 0:
     info = return_currency_data('PLN')
     print(info)
 
-    rate_PLN = f"{info[0]}"
-    req_time = f"{info[1]}"
-    date = f"{info[2]}"
-
     write_csv()
+    date = f"{info[0]}"
+    rate_PLN = f"{info[1]}"
+    req_date = f"{info[3]}"
+    req_time = f"{info[2]}"
+
+    #
     print(f'''
     -----------------------------------------
     Kurs EUR do PLN z dnia {date} wynosi {rate_PLN}
+    Zapytanie z dnia {req_date}
     Czas trwania zapytania wyniósł {req_time} s.
-    -----------------------------------------''')
+        -----------------------------------------''')
 
     time.sleep(5)
